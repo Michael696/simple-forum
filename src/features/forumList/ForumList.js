c2caeaca6d78b963ad2fff0400e1244524bf970c
michael <ms_t@mail.ru>
2022-01-31 12:26:39 +0500

    updated React;

--- src/features/forumList/ForumList.js
+++ src/features/forumList/ForumList.js
@@ -1,12 +1,16 @@
 import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
+import Button from 'react-bootstrap/Button';
+
 import {
+    set,
     selectForumList,
 } from './forumListSlice';
 import ForumItem from './ForumItem';
 
 export default function ForumList() {
     const forumList = useSelector(selectForumList);
+    const dispatch = useDispatch();
 
     const forums = forumList.map(it => {
         return <ForumItem key={it} name={it}/>
@@ -14,5 +18,6 @@ export default function ForumList() {
     return (<div className='main-forums'>
         <div>Forums</div>
         {forums}
+        <Button onClick={() => dispatch(set([1, 2, 3, 4]))}>click</Button>
     </div>);
 }
