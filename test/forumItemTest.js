09134a1bf8c6f133b83f0dcfa2d89396bda687ed
michael <ms_t@mail.ru>
2022-05-17 14:58:39 +0500

    preparing for testing;

--- test/forumItemTest.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render, screen} from '@testing-library/react'
-import ForumItem from "../src/features/forumsList/ForumItem.tsx";
-
-test('test title', () => {
-    render(<ForumItem id={1}/>);
-    expect(screen.getByRole('heading')).toHaveTextContent('Welcome, John Doe');
-})
