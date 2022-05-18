00d7ceb945c3b27eb67cca670ee105d967d058e7
michael <ms_t@mail.ru>
2022-05-18 15:57:58 +0500

    added Threads tests (!!! with dispatch !!!);

--- src/features/threads/Threads.test.tsx
+++ src/features/threads/Threads.test.tsx
@@ -12,7 +12,7 @@ import {setupServer} from "msw/node";
 import {rest} from 'msw';
 import Threads from "./Threads";
 import threadsReducer from "./threadsSlice";
-import currentUserReducer from "../currentUser/currentUserSlice";
+import currentUserReducer, {checkAuth} from "../currentUser/currentUserSlice";
 import {url} from "../../app/urls";
 import {User} from "../../app/types";
 
@@ -88,5 +88,75 @@ test('Threads: has threads in forum', async () => {
     expect(await screen.findByText(/posts:10/)).toBeInTheDocument();
     expect(await screen.findByText(/views:123/)).toBeInTheDocument();
     expect(await screen.findByText(/2022-03-03T16:18:20/)).toBeInTheDocument();
+    server.close();
+});
+
+test.only('Threads: should show "create thread" button for authenticated user', async () => {
+    const user: User = {
+        id: undefined, isAdmin: false, isBanned: false, location: "", name: "", posts: 0, realName: "", registeredAt: ""
+    };
+    const server = setupServer(
+        rest.post('http://127.0.0.1:1337/api/threads', (req, res, ctx) => {
+            return res(
+                ctx.json([{
+                        id: 't01',
+                        forumId: 'f01',
+                        author: user,
+                        title: 'forum id1 thread title 1',
+                        postCount: 10,
+                        viewCount: 123,
+                        likes: [],
+                        dislikes: [],
+                        lastMessage: {
+                            dateTime: '2022-03-03T16:18:20', user
+                        },
+                    }]
+                ),
+            )
+        }),
+    );
+
+    server.use(
+        rest.post('http://127.0.0.1:1337/api/current-user', ((req, res, context) => {
+            return res(
+                context.json({
+                        id: '02',
+                        name: 'user2',
+                        realName: ' user2 real name',
+                        registeredAt: '2001-01-01',
+                        eMail: 'mailB@mail.su',
+                        posts: 888,
+                        location: 'home',
+                        isBanned: false
+                    }
+                )
+            );
+        }))
+    );
+
+    server.listen();
+
+    const store = configureStore({
+        reducer: {
+            threads: threadsReducer,
+            currentUser: currentUserReducer,
+        },
+    });
+
+    await checkAuth()(store.dispatch);
+
+    act(() => {
+        render(
+            <Provider store={store}>
+                <MemoryRouter initialEntries={[`${url.FORUM}/f01`]}>
+                    <Routes>
+                        <Route path={`${url.FORUM}/:forumId`} element={<Threads/>}/>
+                    </Routes>
+                </MemoryRouter>
+            </Provider>
+        );
+    });
+
+    expect(await screen.findByText(/create thread/)).toBeInTheDocument();
     server.close();
 });
